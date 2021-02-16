1f98bf03f0320aa608cd104257bc34b0f7eee913
Simone Totino <totino.simone@gmail.com>
2021-02-16 21:00:42 +0200

    implemented all the CRUD actions and resolved an issue with JS and M1

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,47 @@
 class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task_upd = Task.find(params[:id])
+    @task_upd.update(task_params)
+    redirect_to task_path(@task_upd)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to root_path
+  end
+
+
+
+
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'tasks', to: 'tasks#index', as: :root
+  get 'tasks/new', to: 'tasks#new', as: :task_new
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :task_edit
+  patch 'tasks/:id', to: 'tasks#update', as: :task_update
+  delete 'tasks/:id', to: 'tasks#destroy'
+
+  # put 'tasks/:id', to: 'tasks#update', as: :task_update
+
 end
